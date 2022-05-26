d32c128429e764cdad995eb8a226c817f4728a39
Alex <a.m.mubinov@yandex.ru>
2022-05-26 23:36:45 +0500

    homework_15_2

--- temp/script/script.js
+++ temp/script/script.js
@@ -1,14 +1,14 @@
 let a=42
-alert(typeof a) //number
+console.log(typeof a) //number
 
 let b=true
-alert(typeof b); //boolean 
+console.log(typeof b); //boolean 
 
 let c ='строка'
-alert(typeof c); //string
+console.log(typeof c); //string
 
 d=undefined
-alert(typeof d) //undefined
+console.log(typeof d) //undefined
 
 
 
@@ -28,17 +28,35 @@ do {console.log(e);
 for (var f = 0; f < 9; f++) {
     console.log(f); }
 
-    let UserAge= 20;
+    let UserAge= 22;
 
     switch(UserAge) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
         case 6:
-        alert('Привет ребенок!')
+      console.log('Привет ребенок!')
         break;
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
         case 14:
-        alert('Привет подросток!')
+          console.log('Привет подросток!')
         break;
-        case 22:
-        alert('Привет Юноша!')
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+          console.log('Привет Юноша!')
         break;
     }
    
