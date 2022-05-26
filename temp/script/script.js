61354af9ecd08c382bbb2f638f1de39119c346b6
Alex <a.m.mubinov@yandex.ru>
2022-05-26 19:49:35 +0500

    homework_15

---
+++ temp/script/script.js
@@ -0,0 +1,61 @@
+/*let a=42
+alert(typeof a)*/ //number
+
+/*let b=true
+alert(typeof b);*/ //boolean 
+
+/*let c ='строка'
+alert(typeof c);*/ //string
+
+/*d=undefined
+alert(typeof d)*/ //undefined
+
+
+
+
+/*let a=1    //циклы
+while (a < 10) {
+    i++ ;
+    console.log(a);
+    debugger;
+} */
+
+/*let a=2
+do {console.log(a);
+    debugger;
+    a++;
+}while (a<10)*/
+
+/*for (var a = 0; a < 9; a++) {
+    console.log(a); debugger;}*/
+
+    let UserAge= 14;
+
+    switch(UserAge) {
+        case 6:
+        alert('Привет ребенок!')
+        break;
+        case 14:
+        alert('Привет подросток!')
+        break;
+        case 22:
+        alert('Привет Юноша!')
+        break;
+    }
+   
+
+  /*  let a = 4;
+
+switch (a) {
+  case 3:
+    alert( 'Маловато' );
+    break;
+  case 4:
+    alert( 'В точку!' );
+    break;
+  case 5:
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( 'Нет таких значений' );
+    debugger;} */
