bb2b34cc5f7c5bbbbb1ae508603b30a1e01a859f
Alex <a.m.mubinov@yandex.ru>
2022-05-31 13:39:21 +0500

    homework_17

---
+++ temp/script/script17.js
@@ -0,0 +1,37 @@
+/*const text = document.getElementById('banner__item') ; //получить элемент по id
+console.log(text) ;
+
+const myList = document.querySelectorAll('li') ; //Получить все элементы списка
+console.log(myList) ;
+
+const first = document.querySelector('li') ; //Получить первый элемент списка
+console.log(first) ;
+
+const parent = text.closest('.banner') ;//Получить родителя первого элемента списка
+console.log(parent) ;*/
+
+const node = document.querySelector('p'); //добавить текст
+node.innerHTML = '<p>Привет мир!</p>';
+//node.innerText = 'Привет';
+console.log(node);
+
+ 
+
+/*const  box = document.body ;
+const pos = document.createElement('span') ;//Создать пустой html-узел для абзаца
+pos.innerText = 'Привет! Это добавленный текст!' ; //добавить в него любой текст
+box.append(pos) ;//Добавить созданный абзац последним элементом в body
+
+pos.classList.add('new-class', 'new-class2') ;// Добавить два любых класса
+console.log(pos.classList) ;
+
+pos.classList.remove('new-class2') ; //Удалить класс
+console.log(pos.classList) ;
+
+pos.style.color = 'gold' ;
+
+const bcolor = document.querySelector('ul') ; //изменить цвет текста для всех элементов списка
+bcolor.style.color = 'red';
+bcolor.style.backgroundColor = 'blue';*/
+
+
