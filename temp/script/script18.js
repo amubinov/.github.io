a854e3b4012b9c01c6833b8e0a49fe38e063a636
Alex <a.m.mubinov@yandex.ru>
2022-06-01 01:00:07 +0500

    homework

--- temp/script/script18.js
+++ temp/script/script18.js
@@ -1,5 +1,19 @@
-const btn = document.querySelector('key');
-btn.addEventListener('click','btnClickHandler')
-function btnClickHandler() {
-    alert('кликнули по кнопке!')
+window.addEventListener('load', function() {})
+
+const box = document.querySelector('p'); //click блок анимировано 
+box.addEventListener('click',boxClickHandler); //увеличивал свои 
+
+function boxClickHandler() { //размеры и менял цвет и положение.
+    box.classList.add('Effect')
 }
+     
+const form = document.querySelector('form');
+form.addEventListener('submit', formSubmitHandler);
+function formaSubmitHandler(e) {
+    e.preventDefault();
+    console.log('Отправка формы')
+}
+
+
+
+
