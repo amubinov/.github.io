27fc649f8c3a908ae0f51b7e66407e553307825e
Alex <104054276+amubinov@users.noreply.github.com>
2022-06-23 15:43:37 +0500

    Add files via upload

--- temp/script/script18.js
+++ temp/script/script18.js
@@ -6,13 +6,30 @@ box.addEventListener('click',boxClickHandler); //увеличивал свои
 function boxClickHandler() { //размеры и менял цвет и положение.
     box.classList.add('Effect')
 }
+
+box.addEventListener("click", function(event) {   
+    console.log("Координаты:" + event.clientX +":" + event.clientY);
+})
+
+box.addEventListener('submit', handler);
+console.log(this)
+
+
+function handler(event) {
+    event.preventDefault();
+
+    console.log(event.target)
+}
+
+
+
      
-const form = document.querySelector('form');
+/*const form = document.querySelector('form');
 form.addEventListener('submit', formSubmitHandler);
 function formaSubmitHandler(e) {
     e.preventDefault();
     console.log('Отправка формы')
-}
+}*/
 
 
 
---
+++ temp/script/scripttest.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    userName = 'Alex';
+    text = 'Hi';
+    message = text + ',' + userName;
+}
+
+//showMessage();
+//console.log(message);
+
+function showAgeMessage() {
+    text = 'How many years?'
+    message = userName + ',' + text;
+}
+
+showMessage();
+showAgeMessage();
+console.log(message);
