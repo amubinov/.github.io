cbd421feccf94a6fe0f61be23c18573893098af0
alex <a.m.mubinov@yandex.ru>
2022-08-11 16:18:12 +0500

    education

--- testJS/script/script.js
+++ testJS/script/script.js
@@ -2,3 +2,11 @@ let name = prompt('как тебя зовут?');
 alert(`Тебя зовут ${name} !`);
 let age = prompt(`Сколько тебе лет ${name} ?`)
 alert(`${name} Ты родился в ${2022-age} году!`)
+let geek = confirm('Я прав?');
+if 
+    (geek==true) {alert('Its cool!Thanks!');
+}
+
+
+
+
