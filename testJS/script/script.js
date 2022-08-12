792aafc12702c297af764c29345b6349e04e1ab8
alex <a.m.mubinov@yandex.ru>
2022-08-12 14:27:55 +0500

    java_script

--- testJS/script/script.js
+++ testJS/script/script.js
@@ -2,7 +2,7 @@
 let name = prompt('как тебя зовут?');
 alert(`Тебя зовут ${name} !`);
 let age = prompt(`Сколько тебе лет ${name} ?`)
-alert(`${name} Ты родился в ${new Date().getFullYear() as-age} году!`)
+alert(`${name} Ты родился в ${new Date().getFullYear()-age} году!`)
 let geek = confirm('Я прав?');
 if 
     (geek==true) {alert('Its cool!Thanks!');
