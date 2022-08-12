5518ac4241c37bace07d0fce2dd5a464ae299263
alex <a.m.mubinov@yandex.ru>
2022-08-12 14:20:55 +0500

    java

--- testJS/script/script.js
+++ testJS/script/script.js
@@ -1,12 +1,29 @@
+
 let name = prompt('как тебя зовут?');
 alert(`Тебя зовут ${name} !`);
 let age = prompt(`Сколько тебе лет ${name} ?`)
-alert(`${name} Ты родился в ${2022-age} году!`)
+alert(`${name} Ты родился в ${new Date().getFullYear() as-age} году!`)
 let geek = confirm('Я прав?');
 if 
     (geek==true) {alert('Its cool!Thanks!');
 }
 
+// let question = prompt( 'Какое официальное название javascript?');
+// if (question == 'ECMAScript') {
+//     alert('Верно') ;
+// } else {
+//     alert('Не знаете javascript!')
+// }
 
+// let number = prompt('Введите число!');
+// if (number > 0) {
+//     alert ('Вы ввели число больше 0');
+// } else if (number < 0) {
+//     alert('Вы ввели чило меньше 0')
+// } else {
+//     alert ('Вы ввели число равное 0')
+// }
 
-
+// if (1||0) {
+//     console.log('Правда');
+// }
