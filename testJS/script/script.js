ab4900eca2ad25a56211cd670a60148a46681821
Alex <a.m.mubinov@yandex.ru>
2022-09-01 21:57:02 +0500

    2

--- testJS/script/script.js
+++ testJS/script/script.js
@@ -7,24 +7,3 @@ let geek = confirm('Я прав?');
 if 
     (geek==true) {alert('Its cool!Thanks!');
 }
-
-// let question = prompt( 'Какое официальное название javascript?');
-// if (question == 'ECMAScript') {
-//     alert('Верно') ;
-// } else {
-//     alert('Не знаете javascript!')
-// }
-
-// let number = prompt('Введите число!');
-// if (number > 0) {
-//     alert ('Вы ввели число больше 0');
-// } else if (number < 0) {
-//     alert('Вы ввели чило меньше 0')
-// } else {
-//     alert ('Вы ввели число равное 0')
-// }
-
-// if (1||0) {
-//     console.log('Правда');
-// }
-
